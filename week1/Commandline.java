19998e9e09724574e4cbb22da0c6e65090a7d4d6
Vedansh Shinde <vedanshshinde1@gmail.com>
2025-02-17 10:55:55 +0530

    Add files via upload
    Write a java program to take input as a command line argument. Your name, course, universityrollno and semester. Display the information.
    Name:
    UniversityRollNo:
    Course:
    Semester:

---
+++ week1/Commandline.java
@@ -0,0 +1,13 @@
+
+public class Commandline {
+
+	public static void main(String[] args) {
+		System.out.println("Command line arguments\n");
+		System.out.println("Number of commands - " + args.length+"\n");
+		System.out.println("Name - "+args[0]+"\n");
+		System.out.println("Roll Number - " + args[1]+"\n");
+		System.out.println("Course - "+args[2]+"\n");
+		System.out.println("Semester - " + args[3]+"\n");
+	}
+
+}
